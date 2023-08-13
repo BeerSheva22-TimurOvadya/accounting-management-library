681b986e5a02d0a1a6251cf27b652eefa2c228ef
Ovadya33 <timurgts2710@gmail.com>
2023-08-13 10:43:26 +0300

    Tests accounts

--- src/main/java/telran/spring/AccountController.java
+++ src/main/java/telran/spring/AccountController.java
@@ -1,13 +1,6 @@
 package telran.spring;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,32 +10,38 @@ import telran.spring.security.dto.Account;
 @RestController
 @RequestMapping("accounts")
 @RequiredArgsConstructor
+
 public class AccountController implements AccountService {
 final AccountService accountService;
 final PasswordValidator passwordValidator;
 	@Override
 	@GetMapping("{username}")
 	public Account getAccount(@PathVariable String username) {
+		
 		return accountService.getAccount(username);
 	}
 
 	@Override
 	@PostMapping
 	public void addAccount(@RequestBody @Valid Account account) {
-//		accountService.addAccount(account);
+		passwordValidator.validate(account.getPassword());
+		accountService.addAccount(account);
 
 	}
 
 	@Override
 	@PutMapping("{username}")
-	public void updatePassword(@PathVariable String username, String newPassword) {
-//		accountService.updatePassword(username, newPassword);
+	public void updatePassword(@PathVariable String username, @RequestBody String newPassword) {
+		passwordValidator.validate(newPassword);
+		accountService.updatePassword(username, newPassword);
+
 	}
 
 	@Override
 	@DeleteMapping("{username}")
 	public void deleteAccount(@PathVariable String username) {
-//		accountService.deleteAccount(username);
+		accountService.deleteAccount(username);
+
 	}
 
 }
--- src/main/java/telran/spring/security/AccountServiceImpl.java
+++ src/main/java/telran/spring/security/AccountServiceImpl.java
@@ -1,47 +1,49 @@
 package telran.spring.security;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
+
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import telran.spring.exceptions.NotFoundException;
 import telran.spring.security.dto.Account;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class AccountServiceImpl implements AccountService {
-	final PasswordEncoder passwordEncoder;
-	final AccountProvider provider;
-	ConcurrentHashMap<String, Account> accounts = new ConcurrentHashMap<>();
-
-	@Value("${app.expiration.period:600}")
-	long expirationPeriodHours;
-
-	@Value("${app.security.passwords.limit:3}")
-	int limitPasswords;
-
-	@Autowired
-	UserDetailsManager userDetailsManager;
-
-	@Value("${app.security.validation.period:3600000}")
+	 final PasswordEncoder passwordEncoder;
+     final AccountProvider provider;
+     ConcurrentHashMap<String, Account> accounts = new ConcurrentHashMap<>();
+     @Value("${app.expiration.period:600}")
+     long expirationPeriod;
+     @Value("${app.security.passwords.limit:3}")
+		int limitPasswords;
+     @Autowired
+      UserDetailsManager userDetailsManager;
+     @Value("${app.security.validation.period:3600000}")
 	private long validationPeriod;
-
+     @Value("${app.security.validation.time.unit:HOURS}")
+     private ChronoUnit unit;
+     
+     
+     
 	@Override
 	public Account getAccount(String username) {
 		Account res = accounts.get(username);
 		if (res == null) {
-			throw new NotFoundException(username + "not found");
+			throw new NotFoundException(username + " not found");
 		}
 		return res;
 	}
@@ -52,27 +54,35 @@ public class AccountServiceImpl implements AccountService {
 		if (userDetailsManager.userExists(username)) {
 			throw new IllegalStateException(String.format("user %s already exists", username));
 		}
-		if (accounts.containsKey(username)) {
-			throw new RuntimeException("Error of synchronization between account and accounts manager");
+		if(accounts.containsKey(username)) {
+			throw new RuntimeException("error of synchronization between accounts and accounts manager");
 		}
+		
 		String plainPassword = account.getPassword();
 		String passwordHash = passwordEncoder.encode(plainPassword);
 		Account user = new Account(username, passwordHash, account.getRoles());
-		LocalDateTime ldt = LocalDateTime.now().plusHours(expirationPeriodHours);
+		
+		LocalDateTime ldt = getExpired();
 		user.setExpDate(ldt);
+		
 		LinkedList<String> passwords = new LinkedList<>();
 		passwords.add(passwordHash);
 		user.setPasswords(passwords);
+		
+		
 		createUser(user);
 		log.debug("created user {}", username);
+		
+	}
 
+	private LocalDateTime getExpired() {
+		return LocalDateTime.now().plus(expirationPeriod, unit);
 	}
 
 	private void createUser(Account user) {
-		accounts.putIfAbsent(user.getUsername(), user);
-		userDetailsManager.createUser(User.withUsername(user.getUsername()).password(user.getPassword())
-				.roles(user.getRoles()).accountExpired(LocalDateTime.now().compareTo(user.getExpDate()) <= 0).build());
-
+		accounts.putIfAbsent(user.getUsername(), user) ;
+		userDetailsManager.createUser(createUserDetails(user));
+		
 	}
 
 	@Override
@@ -82,28 +92,37 @@ public class AccountServiceImpl implements AccountService {
 			throw new NotFoundException(username + " not found");
 		}
 		updateUser(account, newPassword);
-		log.debug("Updated user {}", username);
+		log.debug("use {} updated", username);
+
 	}
 
 	private void updateUser(Account account, String newPassword) {
-		if (account.getPasswords().stream().anyMatch(hash -> passwordEncoder.matches(newPassword, hash))) {
-			throw new IllegalStateException("Mismatches Passwords Strategy");
+		if (account.getPasswords().stream()
+				.anyMatch(hash -> passwordEncoder.matches(newPassword, hash))) {
+			throw new IllegalStateException("mismatches Passwords Strategy");
 		}
 		LinkedList<String> passwords = account.getPasswords();
 		String hashPassword = passwordEncoder.encode(newPassword);
+		
 		if (passwords.size() == limitPasswords) {
 			passwords.removeFirst();
 		}
 		passwords.add(hashPassword);
 		String username = account.getUsername();
-		String[] roles = account.getRoles();
+		String[]roles = account.getRoles();
 		Account newAccount = new Account(username, hashPassword, roles);
-		LocalDateTime expTime = LocalDateTime.now().plusHours(expirationPeriodHours);
 		newAccount.setPasswords(passwords);
+		LocalDateTime expTime = getExpired();
 		newAccount.setExpDate(expTime);
 		accounts.put(username, newAccount);
-		userDetailsManager.updateUser(User.withUsername(username).password(hashPassword).roles(roles)
-				.accountExpired(LocalDateTime.now().compareTo(expTime) <= 0).build());
+		userDetailsManager.updateUser(createUserDetails(newAccount));
+		
+	}
+
+	private UserDetails createUserDetails(Account account) {
+		return User.withUsername(account.getUsername())
+				.password(account.getPassword()).roles(account.getRoles())
+				.accountExpired(isExpired(account)).build();
 	}
 
 	@Override
@@ -116,15 +135,15 @@ public class AccountServiceImpl implements AccountService {
 		log.debug("user {} deleted", username);
 
 	}
-
 	@PostConstruct
 	void restoreAccounts() {
 		Thread thread = new Thread(() -> {
-			while (true) {
+			while(true) {
+			
 				try {
 					Thread.sleep(validationPeriod);
 				} catch (InterruptedException e) {
-
+					
 				}
 				expirationValidation();
 			}
@@ -134,20 +153,30 @@ public class AccountServiceImpl implements AccountService {
 		List<Account> listAccounts = provider.getAccounts();
 		listAccounts.forEach(a -> createUser(a));
 	}
-
 	private void expirationValidation() {
-		log.debug("expiration validation");
-		accounts.values().stream().filter(this::isExpired).forEach(
-				a -> userDetailsManager.updateUser(User.withUsername(a.getUsername()).accountExpired(true).build()));
+		
+		int[] count = {0};
+		accounts.values().stream().filter(this::isExpired).forEach(a -> {
+			log.debug("account {} expired", a);
+				userDetailsManager.updateUser(User.withUsername(a.getUsername()).password(a.getPassword())
+						.accountExpired(true).build());
+				count[0]++;
+		});
+		log.debug("expiration validation {} accounts have been expired", count[0]);	
+		
+		
 	}
 
 	private boolean isExpired(Account a) {
-		return LocalDateTime.now().isBefore(a.getExpDate());
+		
+		return LocalDateTime.now().isAfter(a.getExpDate());
 	}
 
 	@PreDestroy
 	void saveAccounts() {
 		provider.setAccounts(new LinkedList<>(accounts.values()));
 	}
+	
+	
 
 }
